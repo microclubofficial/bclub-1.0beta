7b5faf7c16061722f38153dfeffc87a1d466ba1d
caoairong <1101812305@qq.com>
2018-03-28 13:04:37 +0800

    '前端代码'

---
+++ src/views/BibarFirst/BibarChart/ETH.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="eth">
+   这是以太坊页面
+  </div>
+</template>
+
+<script>
+export default{
+  data: function () {
+    return {
+
+    }
+  },
+  mounted: function () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/homePage/HomePageList/baike.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    这是百科页面
+  </div>
+</template>
+
+<script>
+export default{
+  data: function () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
