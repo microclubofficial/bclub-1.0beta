eda24105834c35867c6ed451b7bc20683fefc1fc
shepherd_dc <shepherd_dc@aliyun.com>
2018-03-30 15:56:34 +0800

    online test

--- src/mock/index.js
+++
@@ -1,7 +0,0 @@
-import 'es6-promise'
-import FetchMock from 'mockjs'
-import URL from 'src/api/login'
-import LoginMockData from './login'
-
-// 登录相关
-FetchMock.mock(URL.login, LoginMockData[URL.login]())
