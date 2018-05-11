de20ae150acc7d0f63f500795fc28f604770b045
shepherd_dc <shepherd_dc@aliyun.com>
2018-05-11 19:55:17 +0800

    static page

--- frontend/src/views/login/register.vue
+++ frontend/src/views/login/register.vue
@@ -1,84 +1,134 @@
+<style lang="scss" scoped>
+  .login-box{
+    width:40%;
+    margin:100px auto;
+    border:none;
+    .login-title{
+      padding:20px;
+      background-color: #1e8fff;
+      h3{
+        font-size:24px;
+        letter-spacing: 2px;
+      }
+    }
+    .panel-body{
+      padding:30px 0;
+      .control-label{
+        text-align: left;
+        font-size:14px;
+      }
+    }
+    #register{
+      background-color: #1e8fff;
+      border:none;
+      height:40px;
+      line-height: 40px;
+      font-size:18px;
+      margin:25px 0;
+      &:hover{
+        color:#fff;
+        background-color: #50a6fc;
+      }
+    }
+  }
+  .captcha-box{
+    position: relative;
+    .getcontrol{
+      cursor: pointer;
+      position: absolute;
+      right:15px;
+      top:0;
+      color: #1e8fff;
+      width:41%;
+      height:32px;
+      line-height: 32px;
+      text-align: right;
+      span{
+        color:#333;
+      }
+      i{
+        color:#e8e8e8;
+        font-size:18px;
+        padding:0 10px;
+      }
+    }
+    .disable{
+      color:#e8e8e8;
+    }
+  }
+  .go-login{
+    text-align: center;
+    .login-hover{
+      cursor: pointer;
+      &:hover{
+        color:#1e8fff;
+      }
+    }
+  }
+  .prompt{
+    float: left;
+    margin-left: 4%;
+    margin-top: 10px;
+    color: red;
+  }
+</style>
 <template>
   <div class="login-container">
-    <div class="panel panel-primary" style="width:30%;margin:100px auto">
-      <div class="panel-heading">
-        <a href="/login" style="color:#fff">Register</a>
+    <div class="panel panel-primary login-box">
+      <div class="panel-heading login-title">
+        <h3 class="panel-title text-center">用户注册</h3>
       </div>
       <div class="panel-body">
         <form action="" class="form-horizontal" style="max-width:420px;margin:auto" name="vForm" method="post">
           <div class="form-group">
-            <label class="col-sm-2 control-label">用户名:</label>
-            <div class="col-sm-9">
-              <input  class="form-control" name="username" type="text" placeholder="Username" @blur='showRegisterMsg(userForm.username, 0)'  v-model="userForm.username">
+            <label class="col-md-3 control-label"><span class="text-red">* </span>用户名:</label>
+            <div class="col-md-9">
+              <input  class="form-control" name="username" type="text" placeholder="请输入用户名" @blur='showRegisterMsg(userForm.username, 0)'  v-model="userForm.username">
             </div>
-            <label class="col-sm-2 control-label"></label>
+            <label class="col-md-3 control-label"></label>
             <p class="prompt">{{unamePrompt}}</p>
           </div>
           <div class="form-group">
-            <label class="col-sm-2 control-label">密码:</label>
-            <div class="col-sm-9">
-              <input class="form-control" name="password" type="password" placeholder="Password" @blur='showRegisterMsg(userForm.password, 1)' v-model="userForm.password">
+            <label class="col-md-3 control-label"><span class="text-red">* </span>密码:</label>
+            <div class="col-md-9">
+              <input class="form-control" name="password" type="password" placeholder="请输入密码" @blur='showRegisterMsg(userForm.password, 1)' v-model="userForm.password">
             </div>
-            <label class="col-sm-2 control-label"></label>
+            <label class="col-md-3 control-label"></label>
             <p class="prompt">{{upwdPrompt}}</p>
           </div>
           <div class="form-group">
-            <label class="col-sm-2 control-label">确认密码:</label>
-            <div class="col-sm-9">
-              <input class="form-control" name="repassword" type="password" placeholder="Repassword" @blur='showRegisterMsg(userForm.confirm_password, 2)' v-model="userForm.confirm_password">
+            <label class="col-md-3 control-label"><span class="text-red">* </span>确认密码:</label>
+            <div class="col-md-9">
+              <input class="form-control" name="repassword" type="password" placeholder="请确认密码" @blur='showRegisterMsg(userForm.confirm_password, 2)' v-model="userForm.confirm_password">
             </div>
-            <label class="col-sm-2 control-label"></label>
+            <label class="col-md-3 control-label"></label>
             <p class="prompt">{{confirm_upwdPrompt}}</p>
           </div>
-          <!-- <div class="form-group">
-            <label class="col-sm-2 control-label">邮箱:</label>
-            <div class="col-sm-9">
-              <input class="form-control" name="emai" type="mail" placeholder="emai" v-model="userForm.email" @blur='showRegisterMsg(userForm.email, 4)'>
-            </div>
-            <p class="prompt">{{emailPrompt}}</p>
-          </div> -->
-
-          <!-- <div class="form-group">
-            <label class="col-sm-2 control-label">验证码:</label>
-            <div class="col-sm-9">
-              <div class="input-group">
-                <span class="input-group-addon" style="padding:0;border-right:none;">
-                  <img ref="captcha" :src="controlImg" alt="验证码" width="90" height="20" @click="changeControl()">
-                </span>
-                <input class="form-control" name="captcha" placeholder="Captcha" type="text" style="border-left:none;" v-model="userForm.captcha" @blur='showRegisterMsg(userForm.captcha, 5)'>
-              </div>
-              <p class="prompt">{{captchaPrompt}}</p>
-            </div>
-          </div> -->
           <div class="form-group">
-            <label class="col-sm-2 control-label">手机号:</label>
-            <div class="col-sm-9">
-              <input class="form-control" name="phone" type="text" placeholder="phone" v-model="userForm.phone" @blur='showRegisterMsg(userForm.phone, 3)'>
+            <label class="col-md-3 control-label"><span class="text-red">* </span>手机号:</label>
+            <div class="col-md-9">
+              <input class="form-control" name="phone" type="text" placeholder="请输入手机号" v-model="userForm.phone" @blur='showRegisterMsg(userForm.phone, 3)'>
             </div>
-            <label class="col-sm-2 control-label"></label>
+            <label class="col-md-3 control-label"></label>
              <p class="prompt">{{phonePrompt}}</p>
           </div>
-                <div class="form-group">
-                    <label for="inputCaptcha3" class="col-md-2 control-label">验证码</label>
-                    <div class="col-md-6">
-                        <input type="text" class="form-control" v-model="userForm.captcha" @blur='showRegisterMsg(userForm.phone, 5)' id="inputCaptcha3" placeholder="请输入验证码">
-                    </div>
-                    <button class="col-md-3 btnm getcontrol" type="button" style="padding: 0 !important;height: 34px;line-height: 34px;" @click="getPhoneControl" v-bind:disabled="hasphone" :class="{disable:hasphone}"><span v-show="hasControl">{{countdown}}</span>{{getcontroltxt}}</button>
-                  <label class="col-sm-2 control-label"></label>
-                  <p class="prompt">{{captchaPrompt}}</p>
-                </div>
           <div class="form-group">
-            <label class="col-sm-2 control-label">邀请码:</label>
-            <div class="col-sm-9">
-              <input class="form-control" name="username" type="text" placeholder="邀请码" v-model="userForm.recommender_code">
+              <label for="inputCaptcha3" class="col-md-3 control-label"><span class="text-red">* </span>验证码：</label>
+              <div class="col-md-9 captcha-box">
+                  <input type="text" class="form-control" v-model="userForm.captcha" @blur='showRegisterMsg(userForm.phone, 5)' id="inputCaptcha3" placeholder="请输入验证码">
+                  <div class="col-md-3 getcontrol" v-bind:disabled="hasphone" :class="{disable:hasphone}" @click="getPhoneControl"><span v-show="hasControl">{{countdown}}</span><i> | </i>{{getcontroltxt}}</div>
+              </div>
+            <label class="col-md-3 control-label"></label>
+            <p class="prompt">{{captchaPrompt}}</p>
+          </div>
+          <div class="form-group">
+            <label class="col-md-3 control-label">&nbsp;&nbsp;&nbsp;邀请码:</label>
+            <div class="col-md-9">
+              <input class="form-control" name="recommender_code" type="text" placeholder="邀请码（选填）" v-model="userForm.recommender_code">
             </div>
           </div>
-
-          <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-9 btnm submitForm" id="register" @click="submitForm">注册
-            </div>
-          </div>
-           <h5 style='text-align:center;cursor: pointer'>已注册？<router-link :to="{path:'/login'}">登录</router-link></h5>
+          <button class="btn btn-primary btn-block" type="button" id="register" @click="submitForm">注 册</button>
+          <h5 class="go-login">已有账号？<router-link class="login-hover" :to="{path:'/login'}">去登录</router-link></h5>
         </form>
       </div>
     </div>
@@ -173,6 +223,7 @@ export default {
     },
     submitForm () {
       post(this.formUrl, this.userForm).then(data => {
+        // console.log(data)
         if (data.resultcode === 0) {
           alert(data.message)
           return
@@ -224,53 +275,3 @@ export default {
   }
 }
 </script>
-
-<style rel="stylesheet/scss" lang="scss">
-  .prompt{
-    float: left;
-    margin-left: 4%;
-    margin-top: 10px;
-    color: red;
-  }
-  .form-group>label{padding: 0 10px;}
-  .disable{
-    background: #BCBCBC !important;
-    color: #797979 !important;
-    border:none !important;
-}
-// .disable:hover{
-//     background: #BCBCBC;
-//     color: #797979;
-// }
-.getcontrol{
-    border-radius: 4px;
-    color: #fff;
-    background-color: #5cb85c;
-    border-color: #4cae4c;
-}
-.btn{
-    display: inline-block;
-    padding: 6px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.42857143;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    -ms-touch-action: manipulation;
-    touch-action: manipulation;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-image: none;
-    border: 1px solid transparent;
-}
-.submitForm{
-    color: #fff;
-    background-color: #337ab7;
-    border-color: #2e6da4;
-}
-</style>
